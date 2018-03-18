9a1140501659179889b9cd6c58215179218d546b
yanggy <yangguiyun250725@163.com>
2018-03-18 19:41:29 +0800

    remove open key word

--- src/main/kotlin/com/yanggy/kothlin/SpringbootKothlinApplication.kt
+++ src/main/kotlin/com/yanggy/kothlin/SpringbootKothlinApplication.kt
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
+/**
+ * @author derrick.yang
+ *
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 class SpringbootKothlinApplication
